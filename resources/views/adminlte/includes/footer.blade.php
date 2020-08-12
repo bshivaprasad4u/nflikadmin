7f09333334f87e9eb4fb379fd35636365f9204a2
bshivaprasad4u <bshivaprasad4u@gmail.com>
2020-08-12 13:03:51 +0530

    cors

--- resources/views/adminlte/includes/footer.blade.php
+++ resources/views/adminlte/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2020 <a href="https://nflik.com">CreativeCinema pvt ltd</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.0.1
+        <!-- <b>Version</b> 3.0.1 -->
     </div>
 </footer>
