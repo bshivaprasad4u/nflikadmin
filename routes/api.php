10290c6318da40e98da610563d2883162e9b0bae
bshivaprasad4u <bshivaprasad4u@gmail.com>
2020-08-12 19:07:46 +0530

    cors without middleware

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group([
     'prefix' => 'v1',
-    'middleware' => ['api', 'CORS'],
+    'middleware' => ['api'],
     'namespace' => 'Api\V1'
 
 ], function ($router) {
