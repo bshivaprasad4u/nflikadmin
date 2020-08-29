21ba8a5d282decf83f6aa90e7153fd948c983723
bshivaprasad4u <bshivaprasad4u@gmail.com>
2020-08-29 20:36:26 +0530

    cors

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -15,11 +15,11 @@ class CORS
      */
     public function handle($request, Closure $next)
     {
-        if ($request->expectsJson()) {
-            header('Access-Control-Allow-Origin: *');
-            header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT,  OPTIONS');
-            header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application');
-        }
+        // if ($request->expectsJson()) {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT,  OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application');
+        //}
         return $next($request);
     }
 }
