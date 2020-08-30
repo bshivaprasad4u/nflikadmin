9ce3e4128f6d04c9db1b269fe0ccc1b71b37fda9
bshivaprasad4u <bshivaprasad4u@gmail.com>
2020-08-30 16:27:06 +0530

    payment

--- app/ContentsUser.php
+++ app/ContentsUser.php
@@ -3,13 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ContentsUser extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'user_id', 'content_id'
     ];
-
+    protected $dates = ['deleted_at'];
     function purchased_by_user()
     {
         return $this->hasOne(User::class, 'id', 'user_id');
